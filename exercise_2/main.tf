9e09885413a63b2dc26aca3db01bc756de849c57
Luis Redda <luis.redda@gmail.com>
2020-08-12 14:28:04 -0300

    Update main.tf

--- exercise_2/main.tf
+++ exercise_2/main.tf
@@ -6,9 +6,9 @@ provider "aws" {
 
 terraform {
    backend "s3" {
-   bucket = "rc-tf-remote-state-bucket"
+   bucket = "lr-s3-backend"
    key = "terraform.tfstate"
-   region = "us-east-1"
+   region = "us-east-2"
   }
 }
 
